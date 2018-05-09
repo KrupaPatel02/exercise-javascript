5ce2b02b2e2962bf1b06441b33dc6cc1dd84ba03
Krupa <krupahp90@gmail.com>
2018-05-09 12:13:39 -0400

    first ajax call

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 // User code here
 
 // HINT: start here: https://swapi.co/api/films/
+
+
+$(document).ready(function() {
+  $('#button').click(function() {
+    $.ajax({
+      url: 'https://swapi.co/api/films/',
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        var film = [];
+        for (var i = 0; i < response.results.length; i++){
+          film.push({
+            title: response.results[i].title
+          })
+
+          var dropdownHtml = [];
+          $.each(film, function(i){
+            var newDropdown = "<li id='title'>"+ film[i].title + "</li>"
+            dropdownHtml += newDropdown;
+          })
+
+          $(".dropdown-menu").html(dropdownHtml);
+            }
+          },
+      error: function() {
+        alert('Something went wrong');
+      }
+    });
+  });
+});
