644fe7efae1b1513ccfdf981f2e16eadb6fa4702
Krupa <krupahp90@gmail.com>
2018-05-11 21:47:12 -0400

    js updated

--- js/main.js
+++ js/main.js
@@ -15,18 +15,78 @@ $(document).ready(function() {
         var film = [];
         for (var i = 0; i < response.results.length; i++){
           film.push({
-            title: response.results[i].title
+            title: response.results[i].title,
+            id: response.results[i].title,
+            url: response.results[i].url
           })
 
           var dropdownHtml = [];
           $.each(film, function(i){
-            var newDropdown = "<li id='title'>"+ film[i].title + "</li>"
+            var newDropdown = "<li id="+film[i].id+">"+film[i].title+"</li>"
             dropdownHtml += newDropdown;
           })
 
           $(".dropdown-menu").html(dropdownHtml);
             }
-          },
+
+          var filmData;
+          $("li").click(function () {
+              var id = $(this).text();
+
+              console.log($(this).text());
+              for(var i = 0; i < film.length; i++){
+                if(id === film[i].title){
+                  console.log(film[i].title)
+                  // filmData = JSON.stringify(film[i].url)
+                  filmData = $.getJSON(film[i].url.toString())
+                }
+              }
+              var filmRequest;
+              var filmSelected = [];
+              filmData.done(function(data) {
+
+                for(var i=0; i < data.characters.length; i++){
+                  filmRequest = $.getJSON(data.characters[i].toString(), function(character){
+                    filmSelected.push({name: character.name, starships: character.starships})
+                  })
+                }
+                console.log(filmSelected);
+
+                var shipsFetched;
+                var starships = [];
+                filmRequest.done(function() {
+                    filmSelected.forEach(function(char){
+                      for(var j = 0; j < char.starships.length; j++){
+                        shipsFetched = $.getJSON(char.starships[j], function(data){
+                          starships.push({name: data.name, url: data.url})
+                        })
+                      }
+                    })
+                    console.log("**********************************************");
+                    console.log(starships);
+                    var tablerow;
+                    shipsFetched.done(function() {
+                      filmSelected.forEach(function(char){
+                       for (var i = 0; i < char.starships.length; i++){
+                          starships.forEach(function(ship){
+                            if(ship.url === char.starships[i]){
+                              char.starships[i] = " " + ship.name
+                            }
+                          })
+                        }
+                      })
+                      $.each(filmSelected, function(val){
+                        tablerow += "<tr>"
+                        tablerow += "<td id='charName'>"+ filmSelected[val].name + "</td>";
+                        tablerow += "<td id='starships'>" + filmSelected[val].starships + "</td>";
+                        tablerow += "</tr>"
+                      })
+                      return $("tbody").html(tablerow);
+                    })
+                })
+            })
+        })
+      },
       error: function() {
         alert('Something went wrong');
       }
